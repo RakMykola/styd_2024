5550f2919c2385a4edeffc89aa88b3df751a9276
MykolaRak <natonrak@gmail.com>
2024-02-11 19:32:19 -0600

    all info

---
+++ part1.js
@@ -0,0 +1,54 @@
+From Wikipedia, the free encyclopedia
+"Simpsons" redirects here. For the franchise, see The Simpsons (franchise). For the titular family, see Simpson family. For other uses, see The Simpsons (disambiguation) and Simpson (disambiguation).
+    The Simpsons
+
+Genre
+Animated sitcom
+Satire
+Created by	Matt Groening
+Based on	The Simpsons shorts
+by Matt Groening
+Developed by
+James L. Brooks
+Matt Groening
+Sam Simon
+Voices of
+Dan Castellaneta
+Julie Kavner
+Nancy Cartwright
+Yeardley Smith
+Harry Shearer
+Hank Azaria
+(Complete list)
+Theme music composer	Danny Elfman
+Opening theme	"The Simpsons Theme"
+Ending theme	"The Simpsons Theme"
+Composers	Richard Gibbs (1989–1990)
+Alf Clausen (1990–2017)
+Bleeding Fingers Music (2017–present)
+Country of origin	United States
+Original language	English
+No. of seasons	35
+No. of episodes	760 (list of episodes)
+Production
+Executive producers
+List
+Producers
+Bonita Pietila
+Richard Raynis
+Richard Sakai
+Denise Sirkot
+Running time	21–24 minutes
+Production companies
+Gracie Films
+20th Television[a] (seasons 1–32)
+20th Television Animation (season 33–present)[b]
+Original release
+Network	Fox
+Release	December 17, 1989 –
+present
+The Simpsons is an American animated sitcom created by Matt Groening for the Fox Broadcasting Company.[1][2][3] Developed by Groening, James L. Brooks, and Sam Simon, the series is a satirical depiction of American life, epitomized by the Simpson family, which consists of Homer, Marge, Bart, Lisa, and Maggie. Set in the fictional town of Springfield, it caricatures society, Western culture, television, and the human condition.
+
+    The family was conceived by Groening shortly before a solicitation for a series of animated shorts with producer Brooks. He created a dysfunctional family and named the characters after his own family members, substituting Bart for his own name; he thought Simpson was a funny name in that it sounded similar to "simpleton".[4] The shorts became a part of The Tracey Ullman Show on April 19, 1987. After three seasons, the sketch was developed into a half-hour prime time show and became Fox's first series to land in the Top 30 ratings in a season (1989–1990).
+
+Since its debut on December 17, 1989, 760 episodes of the show have been broadcast. It is the longest-running American animated series, longest-running American sitcom, and the longest-running American scripted primetime television series, both in seasons and individual episodes. A feature-length film, The Simpsons Movie, was released in theaters worldwide on July 27, 2007, to critical and commercial success, with a sequel in development as of 2018. The series has also spawned numerous comic book series, video games, books, and other related media, as well as a billion-dollar merchandising industry. The Simpsons is a joint production by Gracie Films and 20th Television.[5]
